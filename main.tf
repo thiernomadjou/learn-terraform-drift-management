a139d82e2e9be2f288b6c5187d5391dee721241a
Thierno SOW <thiernomadjou@gmail.com>
2024-06-12 15:16:03 +0000

    "

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "example" {
   ami                    = data.aws_ami.ubuntu.id
   key_name               = aws_key_pair.deployer.key_name
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg_ssh.id]
+  vpc_security_group_ids = [aws_security_group.sg_ssh.id, aws_security_group.sg_web.id]
   user_data              = <<-EOF
               #!/bin/bash
               apt-get update
@@ -70,4 +70,18 @@ resource "aws_security_group" "sg_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group" "sg_web" {
+  name        = "sg_web"
+  description = "allow 8080"
+}
+
+resource "aws_security_group_rule" "sg_web" {
+  type              = "ingress"
+  to_port           = "8080"
+  from_port         = "8080"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_web.id
 }
